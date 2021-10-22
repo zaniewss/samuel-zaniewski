2f16b185e83d16042f8f67202f469b0d6822107c
Sam Zaniewski <zaniewss@gmatc.matc.edu>
2021-10-21 22:43:09 -0500

    C

---
+++ 154-assignment-3/Linked_Lists.java
@@ -0,0 +1,58 @@
+public class Linked_Lists {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void displayList(){
+
+    }
+
+    public static void addBeg() {
+
+    }
+
+    public static void remBeg() {
+
+    }
+
+    public static void addEnd() {
+
+    }
+
+    public static void remEnd() {
+
+    }
+
+    public static void addnth() {
+
+    }
+
+    public static void remnth() {
+
+    }
+
+    public static void reverseList() {
+
+    }
+
+    public static void sortData() {
+
+    }
+
+    public static void sortLinks() {
+
+    }
+
+    public static void insertCycle() {
+
+    }
+
+    public static void detectCycle() {
+
+    }
+
+    public static void removeCycle() {
+
+    }
+
+}
